aefc91d0e2f55ed6224a0de0ebedd6d53374469f
Max Lee <maxwell.a.lee56@gmail.com>
2019-04-10 16:53:25 -0400

    Created index page and made it render the home page

--- index.php
+++ index.php
@@ -12,24 +12,11 @@ $f3 = Base::instance();
 
 //define a default route
 $f3->route('GET /', function () {
-    echo '<h1>Hello World!</h1>';
+    //echo '<h1>Hello World!</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
 $f3->run();
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
-
-?>
-</body>
-</html>
